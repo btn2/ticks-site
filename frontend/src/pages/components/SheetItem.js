6ca7580c3c337a1e93620d286227084acf2bbae2
Monko2k <kevinhuang2k@gmail.com>
2021-12-19 04:08:24 -0500

    barebones functioning application

--- frontend/src/pages/components/SheetItem.js
+++ frontend/src/pages/components/SheetItem.js
@@ -1,8 +1,13 @@
+import './SheetItem.css';
 function SheetItem(props) {
+    let time = new Date(props.data.time)
+    let timestring = time.getDate().toString() + '/' + (time.getMonth()+1).toString() + '/' + time.getFullYear().toString()
 
     return (
         <div className="SheetItem">
-            {props.data.nickname} {props.data.quote}
+            <div className="Name">{props.data.nickname}</div>
+            <div className="Quote">"{props.data.quote}"</div>
+            <div className="Time">{timestring}</div>
         </div>
     )
     
